0cd3597b9a5233e8f9bf9320b7aeac6b8545ef96
Alireza-Jodat2001 <AlirezaJodat2001@gmail.com>
2023-05-13 01:21:18 -0700

    Commit after responsive

--- Js/Script.js
+++ Js/Script.js
@@ -1,32 +1,105 @@
-const header = document.querySelector("header") , loader = document.querySelector(".form > input ~ div") , search_shadow = document.querySelector(".search_shadow")
-function log_in_shadow() {
-    if (!search_shadow.classList.contains("d-block")) {
-        search_shadow.classList.add("d-block")
-    }
-    else {
-        search_shadow.classList.remove("d-block")
-    }
-}
-onclick = (elemetn) => {
-    const target = elemetn.target
-    if (document.querySelector(".form").contains(target) || document.querySelector(".search_preset").contains(target)) {
-        header.classList.add("search_active")
-    }
-    else if (!document.querySelector(".search_preset").contains(target) || !document.querySelector(".form").contains(target) || !document.querySelector(".log_in_container").contains(target)) {
-        header.classList.remove("search_active")
-        search_shadow.classList.remove("d-block")
-    }
-}
+const header = document.querySelector("header") , search_shadow = document.querySelector(".search_shadow")
 function set_shadow() {
     const header_height = header.offsetHeight
-    const body_height = document.querySelector("body").offsetHeight
-    search_shadow.style.height = `${body_height - header_height}px`
+    search_shadow.style.height = `${document.querySelector("body").offsetHeight - header_height}px`
+    search_shadow.style.top = `${header_height}px`
 }
-onresize = () => set_shadow()
-onload = () => set_shadow()
+function search_disable() {
+    header.classList.remove("search_active")
+    shadow_disable()
+}
+function shadow_disable() {
+    search_shadow.classList.add("d-none")
+}
+function log_in_onclick() {
+    const log_in_if = (!document.querySelector(".dropdown-menu").classList.contains("show")) ? search_shadow.classList.remove("d-none") : shadow_disable()
+}
+onclick = (event) => {
+    const target = event.target
+    const search_if = (document.querySelector("form").contains(target)) ? header.classList.add("search_active") : search_disable()
+}
+onresize = () => set_shadow(); set_gap_slideshow()
+onload = () => set_shadow(); set_gap_slideshow()
 oninput = () => {
+    const loader = document.querySelector(".form > input ~ div")
     loader.classList.add("loader")
     setTimeout(() => {
         loader.classList.remove("loader")
     }, 800)
 }
+function li_mouseover() {
+    const liElements = document.querySelectorAll('#my-nav > ul > li')
+    for (let i = 0; i < liElements.length; i++) {
+        search_disable()
+        document.querySelector("#log_in ~ .dropdown-menu").classList.remove("show")
+        document.querySelector("form > input").setAttribute("disabled" , "")
+        document.querySelector("form > input").disabled = ""
+    }
+}
+const slide_show = document.querySelector(".slide_show") , address = [] , img_a = []
+for (let i = 0; i < 27; i++) {
+    address[i] = `../Img/Carousel/${i}.png`
+    address.sort(() => Math.random() - 0.5)
+    if (i == 26) {
+        for (let j = 0; j < 20; j++) {     
+            const slideshow_items = `<a href="" class="img_link bg-white d-flex justify-content-center align-items-center border_radius_lg"><img src="${address[j]}" class="img_slide"></a>`
+            img_a[j] = slideshow_items
+            slide_show.insertAdjacentHTML("beforeend" , slideshow_items)
+        }
+    }
+}
+var scroll = 0 , width = 0 , gap = 70
+for (let i = 0; i < 7; i++)
+    slide_show.insertAdjacentHTML("beforeend" , img_a[i])
+var interval = setInterval(slideshow_func , 6000)
+const link_element = document.querySelectorAll(".slide_show > a")
+function slideshow_func() {
+    scroll++
+    if (scroll == 21) {
+        clearInterval(interval)
+        scroll = 1
+        for (let i = 0; i < 27; i++) {
+            link_element[i].classList.add("a_active")
+            document.querySelectorAll(".a_active")[i].style.setProperty("--var-transform" , -width)
+        }
+        setTimeout(() => {
+            for (let i = 0; i < 27; i++)
+                link_element[i].classList.remove("a_active")
+        }, 0)
+        width = 0
+        interval = setInterval(slideshow_func , 6000)
+    }
+    width += document.querySelector(".slide_show > a").offsetWidth + set_gap_slideshow()
+    for (let i = 0; i < 27; i++) {
+        link_element[i].style.transform = `translate(${width}px , 0px)`
+        link_element[i].style.transition = `0.3s`
+    }
+}
+function set_gap_slideshow() {
+    if (window.innerWidth >= 1200)
+        return gap = 70
+    else if (window.innerWidth > 991 && window.innerWidth < 1200)
+        return gap = 44
+    else if (window.innerWidth > 768 && window.innerWidth < 992)
+        return gap = 28
+    else if (window.innerWidth < 769)
+        return gap = document.querySelector(".slide_show").offsetWidth * 0.08
+    else if (window.innerWidth < 519)
+        return gap = document.querySelector(".slide_show").offsetWidth * 0.01
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
